fd6fb3304410541dee7b2cb680dc17ab29d21f8b
tarunKakani <tarunkakani007@gmail.com>
2026-03-24 10:15:35 -0400

    continued file perm scanning

--- src/file_perm_scan.py
+++ src/file_perm_scan.py
@@ -1,33 +1,54 @@
-# - find all files with SUID bit set to 4000 (find / -perm -4000)
-# - these run as root regardless of who executes them and are prime targets for priv escl
-#
 # - scan for files/dirs that are writable by anyone (/tmp is okay sometimes but never in /etc)
-#
-# - all imp and critical file permissions 
-# -- /etc/shadow (must be 600 or 400)
-# -- /etc/passwd (must be 644)
-# -- boot loader config (/boot/grub/grub.cfg)
 #
 # - check if /tmp /var /home are on seperate partitions
 # -- check if /tmp is mounted with noexec and nosuid
 
-
 import subprocess
+import os
+import stat
 
 def find_suid_files():
 
     command = "find / -perm -4000 -type f 2>/dev/null"
     result = subprocess.run(command, shell=True, capture_output=True, text=True)
 
-    if result.returncode == 0:
-        files = result.stdout.strip().split('\n')
+    if result.returncode == 0 or result.returncode == 1:
+        output = result.stdout.strip()
+
+        if output:
+            files = output.split('\n')
+        else:
+            files = []
 
         print(f"Found {len(files)} SUID Binaries. Review these for potential priveledge escalation attacks.")
         
-        for f in files[:files]:
+        for f in files[:5]:
             print(f"    - {f}")
-        print("     ...(truncated)")
+        print("    - (truncated)")
     else:
         print(f"Error running command: {result.stderr}")
 
-find_suid_files()
+def critical_files_check(filepath, expected_params_list):
+    
+    try:
+        file_stat = os.stat(filepath)
+        actual_mode = stat.S_IMODE(file_stat.st_mode)
+        
+        actual_oct = oct(actual_mode)[2:]
+
+        if actual_oct not in expected_params_list:
+            print(f"Not safe: {filepath} has unsafe permissions {actual_oct}")
+        else:
+            print(f"Safe: {filepath} has safe permissions {actual_oct}")
+    except FileNotFoundError:
+        print(f"File does not exist {filepath}")
+    except PermissionError:
+        print(f"You do not have permission to access {filepath}. Run as sudo/root.")
+    
+rules = {"/etc/shadow":['600', '400', '000'],
+        "/etc/passwd":['644'],
+        "/boot/grub/grub.cfg":['600', '400']}
+
+for filepath, expected_params in rules.items():
+    critical_files_check(filepath, expected_params)
+
